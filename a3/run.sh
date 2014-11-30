1a3377b40365cb20e9076e63afa10adcf761aef2
Sourav Chakraborty <chakrabs@cse.osu.edu>
2014-11-30 16:42:27 -0500

    remove older files

--- a3/run.sh
+++
@@ -1,5 +0,0 @@
-./mmdriver -bin k1.bin -block 32 1024 -thread 32 1 -mat 1024 -size 1024 -check
-./mmdriver -bin k2.bin -block 32 1024 -thread 32 1 -mat 1024 -size 1024 -check
-./mmdriver -bin k3.bin -block 16 1024 -thread 32 1 -mat 1024 -size 1024 -check
-./mmdriver -bin k4.bin -block 32 512  -thread 32 1 -mat 1024 -size 1024 -check
-./mmdriver -bin k5.bin -block 16 512  -thread 32 1 -mat 1024 -size 1024 -check
