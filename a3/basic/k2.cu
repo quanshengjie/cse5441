50243ff1246048524286b55247e49c708304a10e
Sourav Chakraborty <chakrabs@cse.osu.edu>
2014-12-01 14:36:11 -0500

    add missing sync in k2

--- a3/basic/k2.cu
+++ a3/basic/k2.cu
@@ -10,10 +10,13 @@ mmkernel( float* a, float* b, float* c,
     __shared__ float cb[512];
 
     float sum = 0.0;
-    for( int ks = 0; ks < p; ks += bx ){
-      cb[tx] = c[ks+tx+pitch_c*j];
-      for( int k = ks; k < ks+bx; ++k )
-        sum += b[i+pitch_b*k] * cb[k-ks];
+    for( int ks = 0; ks < p; ks += bx ) {
+        cb[tx] = c[ks+tx+pitch_c*j];
+        __syncthreads();
+        for( int k = ks; k < ks+bx; ++k ) {
+            sum += b[i+pitch_b*k] * cb[k-ks];
+        }
+        __syncthreads();
     }
     a[i+pitch_a*j] = sum;
 }
