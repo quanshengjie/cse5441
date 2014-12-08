eea62a986def35c1727cd7793ed104ce0814ebf8
Sourav Chakraborty <chakrabs@cse.osu.edu>
2014-12-08 00:36:37 -0500

    add run script

---
+++ a4/solver/RunPA4.sh
@@ -0,0 +1,20 @@
+#PBS -l walltime=1:00:00
+#PBS -l nodes=1:ppn=12
+#PBS -N PA4
+#PBS -S /bin/ksh
+#PBS -j oe
+
+ITER=3
+
+module load mvapich2
+cd $HOME/5441/a4/solver
+
+for np in 1 4 8 12
+do
+   for((i=1;i<=$ITER;i++))
+   do
+        echo "### nprocs=$np iteration=$i"
+        mpiexec -n $np ./solver
+        echo "\n\n"
+   done
+done
